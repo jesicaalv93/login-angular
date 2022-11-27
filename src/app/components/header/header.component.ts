d390a7c960f57bf8364eb7ba47f0cbe662a7bd06
unknown <jesicaalvarez1993@gmail.com>
2022-11-27 20:11:54 -0300

    etodo login y logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  //Inyectamos la dependencia del servicio
+  constructor(private auth:AuthorizationService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public get isLogin(): boolean {
+    return this.auth.isUserLogIn();
+  }
+
+  public btnLogout():void {
+    this.auth.logout();    
+  }
 
   }
--- src/app/services/authorization.service.ts
+++ src/app/services/authorization.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
   private strUrlApi:string;
+  router: any;
 
   constructor(private http:HttpClient) {
     this.strUrlApi = '../../assets/data/user.json';
   }
 
+public loginSimple (email: string, pwd: string): void {
+  this.http.get(this.strUrlApi).subscribe(
+    (response:any) => {
+      if(response.token != null) {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard'])
+      }
+    }
+  );
+}
 
+public logout(): void {
+  //Al cerrar sesion eliminamos el token
+  localStorage.removeItem('token');
+  //Redireccionar
+  this.router.navigate(['/']);
+}
+
+public isUserLogIn(): boolean {
+  return (localStorage.getItem('token') != null);
+}
 
   }
