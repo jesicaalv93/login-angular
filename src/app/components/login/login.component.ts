631e50a0f72f85583b3d1cacf4b1db88a6e8d6e9
unknown <jesicaalvarez1993@gmail.com>
2022-12-04 19:22:56 -0300

    formulario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizationService } from '../../services/authorization.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  public loginTxtEmail:string;
+  public loginTxtPwd:string;
+
+  constructor(private auth:AuthorizationService) { 
+    //Inicializar atributos
+    this.loginTxtEmail = "";
+    this.loginTxtPwd = "";
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public btnLogin(): void {
+    //Consultamos la API
+    this.auth.loginSimple(this.loginTxtEmail, this.loginTxtPwd);
+  }
 
 }
